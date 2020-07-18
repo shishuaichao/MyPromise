c79fc09e69e893f8fb719c15de588978406a1b28
帅超 <shishuaichao@linglie.com>
2020-07-18 13:56:29 +0800

    链式调用

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 
 let MyPromise = require('./myPromise.js')
+// 切换两个方法看效果是否一样
 
 let promise = new MyPromise((resolve, reject) => {
-        setTimeout(() => {
-            // resolve('成功')
-            reject('失败')
-        }, 1000);
-    
+        // setTimeout(() => {
+            resolve('成功')
+            // reject('失败')
+        // }, 1000);
 })
+
+// let promise = new Promise((resolve, reject) => {
+//         // setTimeout(() => {
+//             resolve('成功')
+//             // reject('失败')
+//         // }, 1000);
+// })
 
 promise.then(resolve => {
     console.log(resolve)
+    // return '第一次then'
 }, reason => {
     console.log(reason)
+    // return '第一次then'
 })
+.then(resolve => {
+    console.log(resolve)
+    return new MyPromise((res, rej) => {
+        res('第二次then')
+    })
+}, reason => {
+    console.log(reason)
+    return new MyPromise((res, rej) => {
+        rej('第二次then')
+    })
+})
+.then(resolve => {
+    console.log(resolve)
+    return '第三次then'
+}, reason => {
+    console.log(reason)
+    return '第三次then'
+})
+
+
--- myPromise.js
+++ myPromise.js
@@ -9,7 +9,10 @@
  * 6. 异步实现
  *    产生原因：在调用then的时候状态仍处于pending，尚未调用resolve或者reject，就无法拿到实例调用resolve传进来的参数
  *    解决办法：在pending状态时，把成功回调和失败回调存在变量中，外部调用resolve时，判断变量中是否有值，有值说明是异步，调用成功回调
- * 7. 链式调用。。。待定
+ * 7. 链式调用
+ *    1. then返回的是promise实例，才能继续调用then方法
+ *    2. 成功或者失败回调应该是一个数组，而非一个普通变量，用来存储每一次的回调方法，然后才能依次调用
+ * 8. 异步链式调用。。。待定
  */
 
 const PENDING = 'pendding';
@@ -23,29 +26,46 @@ class MyPromise {
     status = PENDING;
     value = undefined;
     reason = undefined;
-    successCallback = undefined;
-    failCallback = undefined;
+    successCallback = [];
+    failCallback = [];
     resolve = resolve => {
         this.status = FULFILLED;
         this.value = resolve;
-        this.successCallback && this.successCallback(this.value);
+        // 要想实现链式调用，就把所有的then中的成功或失败回调存在一个数组中，每次调用后弹出
+        while (this.successCallback.length) this.successCallback.shift(this.value);
     }
     reject = reason => {
         this.status = FAILED;
         this.reason = reason;
-        this.failCallback && this.failCallback(this.reason);
+        while (this.failCallback.length) this.failCallback.shift(this.reason);
         
     }
     then(successCallback, failCallback) {
-        if (this.status === FULFILLED) {
-            successCallback(this.value);
-        } else if (this.status === FAILED) {
-            failCallback(this.reason);
-        } else {
-            // 异步
-            this.successCallback = successCallback;
-            this.failCallback = failCallback;
-        }
+        // 首先链式调用的前提是必须返回一个自身的promise
+        // 如果回调返回结果不是一个promise，则调用这个promise中的resolve或者reject将返回结果传入
+        // 如果回调中返回一个primise就调用这个promise中的then，然后把对一个的resolve或者reject传入返回的promise中
+        // 这样即使上一个then不返回值，也就是返回的undefined也会执行，不会影响下一次then的调用
+        return new MyPromise((resolve, reject) => {
+            if (this.status === FULFILLED) {
+                let r = successCallback(this.value);
+                if (r instanceof MyPromise) {
+                    r.then(resolve, () => {})
+                } else {
+                    resolve(r)
+                }
+            } else if (this.status === FAILED) {
+                let r = failCallback(this.reason);
+                if (r instanceof MyPromise) {
+                    r.then(() => {}, reject)
+                } else {
+                    reject(r)
+                }
+            } else {
+                // 异步
+                // this.successCallback.push(successCallback);
+                // this.failCallback.push(failCallback);
+            }
+        })
     }
 }
 
